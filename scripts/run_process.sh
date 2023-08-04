ee72f829285f953de07d05e27b8e5c794c26d28d
kyjun14 <131837256+kyjun14@users.noreply.github.com>
2023-08-04 13:53:14 +0900

    Update run_process.sh

--- scripts/run_process.sh
+++ scripts/run_process.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 REGION="ap-northeast-2"
-ECR_REPOSITORY="257307634175.dkr.ecr.ap-northeast-2.amazonaws.com"
-ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/aws13-spring-petclinic"
+ECR_REPOSITORY="257307634175.dkr.ecr.ap-northeast-2.amazonaws.com/project01-spring-petclinic"
+ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/project01-spring-petclinic"
 ECR_DOCKER_TAG="1.0"
 
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ECR_REPOSITORY};
